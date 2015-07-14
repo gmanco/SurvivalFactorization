53ba46822db7b8b3acf77da83be7c02722bfac41
gmanco <manco.giuseppe@gmail.com>
2015-07-14 17:01:57 +0200

    Test class for the distributions

--- src/utils/TestDistributions.java
+++ src/utils/TestDistributions.java
@@ -2,5 +2,11 @@ package utils;
 
 public class TestDistributions {
 	
-	public static void main(String[] args)
+	public static void main(String[] args){
+		int seed = 12345;
+		Randoms r = new Randoms(seed);
+		System.out.println(r.nextGamma(2, 2));
+		
+		
+	}
 }
