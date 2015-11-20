7e28324343a8697729bf3d8069c8817d9c3ca949
gmanco <manco.giuseppe@gmail.com>
2015-11-20 16:50:35 +0100

    refactored

--- src/survivalFactorizationEM/SurvivalFactorizationEM_Configuration.java
+++ src/survivalFactorizationEM/SurvivalFactorizationEM_Configuration.java
@@ -4,11 +4,13 @@ import utils.Randoms;
 
 public class SurvivalFactorizationEM_Configuration {
 
-    public static Randoms randomGen=new Randoms(131);
+    
+	public static Randoms randomGen=new Randoms(131);
     
     public static double eps=0.000001;
     
     public static int DEFAULT_N_ITERATIONS=100;
+    public static final int DEFAULT_N_FACTORS = 2;
     public static int DEFAULT_SAVE_STEP=10;
     public static String DEFAULT_OUTPUT="survFactorization.model";
     
