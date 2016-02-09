cca03725a23f7142d9c7794aff637a42a2893f9c
gmanco <manco.giuseppe@gmail.com>
2016-02-09 19:16:33 +0100

    Minor fixes

--- src/survivalFactorizationEM/SurvivalFactorizationEM_ModelCounters.java
+++ src/survivalFactorizationEM/SurvivalFactorizationEM_ModelCounters.java
@@ -36,57 +36,66 @@ public class SurvivalFactorizationEM_ModelCounters {
 
 	public void update(CascadeData cascadeData, SurvivalFactorizationEM_Model model) {
 		resetCounters();
-        List<CascadeEvent> eventsCurrCascade;
-        Set<Integer> inactiveVertices = new HashSet<Integer>();
+		List<CascadeEvent> eventsCurrCascade;
+		Set<Integer> inactiveVertices = new HashSet<Integer>();
 
-		
-        for(int k=0;k<nFactors;k++){
-        	for (int n = 0; n <nVertices; n++)
-        		S_k[k] = model.S[n][k];            		
-        }
-        
-	       for (int c = 0; c < cascadeData.getNCascades(); c++) {
-	            
-	    	   eventsCurrCascade = cascadeData.getCascadeEvents(c);
-	            inactiveVertices.clear();
-	            inactiveVertices.addAll(cascadeData.getNodeIds());
+		for (int k = 0; k < nFactors; k++) {
+			for (int n = 0; n < nVertices; n++)
+				S_k[k] = model.S[n][k];
+		}
 
-	            for (CascadeEvent currentEvent : eventsCurrCascade) {
-	                for(int k=0;k<nFactors;k++){
-		            		int n = currentEvent.node; 
-		            		double time = currentEvent.timestamp;
-		            		
-		            		A_c_u_k[c][n][k] += model.A[n][k];
-		            		tilde_A_c_u_k[c][n][k] += time*model.A[n][k];
-		            		A_c_k[c][k] += model.A[n][k];
-		            		tilde_A_c_k[c][k] += time*model.A[n][k];
-		            		
-		            		S_c_k[c][k] += model.S[n][k];	
-		            		tilde_S_c_k[c][k] += time*model.S[n][k];
-		            		L_c_k[c][k] += Math.log(model.S[n][k]);
-		            			
-		                inactiveVertices.remove(currentEvent.node);
-		            }
-	            }
-	            // for each inactive node (update S_den)
-	                for (int inactiveNode : inactiveVertices) {
-	                        for (int k = 0; k < model.nFactors; k++) {
-			            		tilde_S_c_k[c][k] += cascadeData.t_max*model.S[inactiveNode][k];
-	                        }
-	                }
-	       }
-	       
-	       for (int c = 0; c < cascadeData.getNCascades(); c++) {
-	            
-	    	   eventsCurrCascade = cascadeData.getCascadeEvents(c);
-	    	   ListIterator<CascadeEvent> li = eventsCurrCascade.listIterator(eventsCurrCascade.size());
-	    	   while(li.hasPrevious()) {
-	    		   CascadeEvent currentEvent = li.previous();
-	    		   for(int k=0;k<nFactors;k++){
-		            		R_c_u_k[c][currentEvent.node][k] += 1/(A_c_u_k[c][currentEvent.node][k]); 
-		            }
-	            }
-	       }
+		for (int c = 0; c < cascadeData.getNCascades(); c++) {
+
+			eventsCurrCascade = cascadeData.getCascadeEvents(c);
+			inactiveVertices.clear();
+			//FIXME: mi sembra estremamente inefficiente. 
+			inactiveVertices.addAll(cascadeData.getNodeIds());
+			CascadeEvent prevEvent = null;
+			for (CascadeEvent currentEvent : eventsCurrCascade) {
+				for (int k = 0; k < nFactors; k++) {
+					int n = currentEvent.node;
+					double time = currentEvent.timestamp;
+
+					if (prevEvent != null) {
+						A_c_u_k[c][n][k] += model.A[prevEvent.node][k] + A_c_u_k[c][prevEvent.node][k];
+						tilde_A_c_u_k[c][n][k] += time * model.A[prevEvent.node][k]
+								+ tilde_A_c_u_k[c][prevEvent.node][k];
+					}
+
+					tilde_A_c_k[c][k] += time * model.A[n][k];
+					A_c_k[c][k] += model.A[n][k];
+					S_c_k[c][k] += model.S[n][k];
+					tilde_S_c_k[c][k] += time * model.S[n][k];
+					L_c_k[c][k] += Math.log(model.S[n][k]);
+
+					inactiveVertices.remove(currentEvent.node);
+					prevEvent = currentEvent;
+				}
+			}
+			// for each inactive node (update S_den)
+			for (int inactiveNode : inactiveVertices) {
+				for (int k = 0; k < model.nFactors; k++) {
+					tilde_S_c_k[c][k] += cascadeData.t_max * model.S[inactiveNode][k];
+				}
+			}
+		}
+
+		for (int c = 0; c < cascadeData.getNCascades(); c++) {
+
+			eventsCurrCascade = cascadeData.getCascadeEvents(c);
+			ListIterator<CascadeEvent> li = eventsCurrCascade.listIterator(eventsCurrCascade.size());
+			CascadeEvent prevEvent = null;
+
+			while (li.hasPrevious()) {
+				CascadeEvent currentEvent = li.previous();
+				if (prevEvent != null)
+					for (int k = 0; k < nFactors; k++) {
+						R_c_u_k[c][currentEvent.node][k] += 1 / (A_c_u_k[c][prevEvent.node][k])
+								+ R_c_u_k[c][prevEvent.node][k];
+					}
+				prevEvent = currentEvent;
+			}
+		}
 	}
 
 	private void resetCounters() {
